1cf424b333d2c696ba03750f9b3034ad8fb7fc2f
Mark Park <markallenpark@gmail.com>
2025-05-21 22:29:13 -0500

    update tabstops

--- init.lua
+++ init.lua
@@ -1,4 +1,11 @@
 require("config.lazy")
 
+-- Set theme
 vim.cmd[[colorscheme tokyonight-moon]]
+
+-- Set tabstops
+vim.o.tabstop = 4
+vim.o.expandtab = true
+vim.o.softtabstop = 4
+vim.o.shiftwidth = 4
 
